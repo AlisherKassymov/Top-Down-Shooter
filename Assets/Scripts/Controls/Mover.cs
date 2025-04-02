7d87823b45ee465975f3d4855fc691abf7ba45b3
Alisher Kassymov <37921839+AlisherKassymov@users.noreply.github.com>
2025-04-02 15:22:59 +0500

    feat: rig aim constraints(TOP-Section4)

--- Assets/Scripts/Controls/Mover.cs
+++ Assets/Scripts/Controls/Mover.cs
@@ -81,7 +81,7 @@ namespace Controls
 
                 transform.forward = _lookingDirection;
 
-                _aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+                _aim.position = new Vector3(hitInfo.point.x, transform.position.y + 1, hitInfo.point.z);
             }
         }
 
