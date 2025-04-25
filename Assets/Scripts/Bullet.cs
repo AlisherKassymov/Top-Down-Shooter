032b1ec82752409bdf7c6d4b54f67bedb692b425
Alisher Kassymov <37921839+AlisherKassymov@users.noreply.github.com>
2025-04-25 15:06:22 +0500

    feat: bullet impact fx

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _bulletLifeTime;
+    [SerializeField] private GameObject _bulletImpactFX;
     private Rigidbody _rb => GetComponent<Rigidbody>();
 
     private void Update()
@@ -15,9 +16,20 @@ public class Bullet : MonoBehaviour
         _bulletLifeTime -= Time.deltaTime;
     }
     
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
+        CreateImpactFX(collision);
         GetComponent<TrailRenderer>().Clear();
         ObjectPool.Instance.ReturnBullet(gameObject);
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contactPoint = collision.contacts[0];
+            GameObject newImpactFX = Instantiate(_bulletImpactFX, contactPoint.point, Quaternion.LookRotation(contactPoint.normal));
+            Destroy(newImpactFX, 1f);
+        }
     }
 }
